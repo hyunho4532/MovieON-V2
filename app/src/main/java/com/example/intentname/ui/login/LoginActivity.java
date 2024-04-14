0ac49b1ebd03704a5f3445e15b43061f46d50170
hyunho4532 <kyounguk192@gmail.com>
2024-04-14 21:37:01 +0900

    2024-04-14 영화 메인 화면 UI 개편 중

--- app/src/main/java/com/example/intentname/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/intentname/ui/login/LoginActivity.java
@@ -1,6 +1,5 @@
-package com.example.intentname.login;
+package com.example.intentname.ui.login;
 
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,24 +11,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.example.intentname.MainActivity;
+import com.example.intentname.ui.MainActivity;
 import com.example.intentname.R;
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
